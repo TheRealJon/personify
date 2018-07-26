b36ce12d79fba531be121294d6a2212364e7a41a
Jon Jackson <jonjacks@redhat.com>
2018-07-26 11:31:23 -0400

    Move API to separate repo, change to react front end

---
+++ src/components/app/app.component.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './app.styles.css';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
